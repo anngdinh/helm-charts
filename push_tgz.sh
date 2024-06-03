42e7bfdb774cc6e3a94beae6d3ee83aecb4709b5
andrew <an.ngdinh@gmail.com>
2024-06-03 16:46:36 +0700

    🚀 test new workflow

--- push_tgz.sh
+++ push_tgz.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 echo "Repo URL: $1";
+echo "Change charts: $2";
 
-# Loop through all .tgz files in the specified folder (replace with your folder)
-for file in ./*.tgz; do
-  # Check if file exists
-  if [ -f "$file" ]; then
-    echo "Pushing: $file"
-    helm push "$file" "$1"
-  fi
+folders=$2
+IFS=',' read -ra folder_array <<< "$folders"
+for folder in "${folder_array[@]}"; do
+  cd $folder
+  rm -rf ./*.tgz
+  echo "Processing chart: $folder"
+  helm package .
+  for file in ./*.tgz; do
+    if [ -f "$file" ]; then
+      echo "Pushing: $file"
+      helm push "$file" "$1"
+    fi
+  done
 done
 
 echo "Push process completed."
